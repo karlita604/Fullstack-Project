a6fad785b031b6fe4fc514a7ae3d8b20d87e89b1
karlita604 <53234902+karlita604@users.noreply.github.com>
2020-05-22 20:29:04 -0700

    Enabled Google OAuth API w client ID & secret

--- index.js
+++ index.js
@@ -1,47 +1,19 @@
+//Client ID: 537955690285-nk4qaitqcm7ke10m4k8o714mhn2lda2s.apps.googleusercontent.com
+//Client Secret: TsWVsbH1SAXNkyCA0QaCAJpN
+
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+//because we only want that specific property
 
 const app = express(); //first express application
 
-/* represents an express application
-this application is used to set up configuration that
- listen to requests coming from the Node side and then route
- the requests to different route handlers */
+passport.use(new GoogleStrategy());
+/*
+passport.use --> generic register
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
-});
+new GoogleStrategy() --> new instance of the passport.Strategy
+*/
 
 const PORT = process.env.PORT || 5000;
-/*
-We have to wait until the app is being executed
-by Heroku when we can look in the underlying environment
-to see if a port has been declared
-
-If we are running in a developping environment this
-variable may not be declared so we need to add the 5000 as default.
-
-Or whatever port Heroku is telling us to listen to.
-*/
-
 app.listen(PORT);
-
-/*
-app -> Express App to register this route handler.
-
-get -> creating a brand new route handlers
-      watch for incoming http requesrs with this method
-      methods are used to indicate a type
-      get info
-'/' -> watch for reuests trying to access '/' :route
-      route portion of the handler
-req -> object representing the incoming requests
-      data about who is making the request
-res -> Object representing the outgoing response
-      data that is about to be sent back for the requests
-res.send({ hi: "there" }); -> immediately close the request
-                            and send some JSOn back to whoever
-                            made this request
-app.listen(5000); -> instructs Express to tell Node that it
-                    wants to listen for incoming traffic on port 5000
-
-*/
